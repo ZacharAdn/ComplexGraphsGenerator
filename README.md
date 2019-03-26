2a460bca226eba5f4ed2a42004efaee0e4435da2
ZacharAdn <zahar91@gmail.com>
2019-03-26 11:52:07 +0200

    connectivity verification, usage and explanation on readme file

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # graphGenerator
 
-Graph generator for graph databases
+## Fraud graph generator for graph databases
 
-Generate edges.csv file for GraphSim and Neo4j graph database, plot an network graph using 'networkx'
+Generate directed, connected, weighted (or not..) graph files for 'GraphSim', 'Neo4j' and Amazons - 'Neptune' graph database platforms<br><br>
+Enable controlling on the graph size, maximum out degree of a vertex in the graph, weights type and out format <br> 
+Plot an network graph using 'networkx' and debugging options allowed
 
-usage: $ python3 graphGenerator.py <n_nodes> <max_degree>
+<pre>
+Usage - python3 graphGenerator.py     -v (num_of_vertices)
+                                      -d (max_degree)
+                                      -s (seed_number)
+                                      -w (boolean - is_weighted)                                (optional, default - unweighted)
+                                      -f (graph_format)   [graphsim, neptune, neo4j]   (optional, default - graphsim)
+
+
+Usage example:
+[zachara@ip-xxx-xx-xx-xxx]$ python3 graphGenerator.py -v 1048576 -d 16 -s 5 -w -f graphsim
+</pre>
--- graphGenerator.py
+++ graphGenerator.py
@@ -4,8 +4,13 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import time
 import collections
+import os.path
 
-# Usage - python3 graphGenerator.py <num_of_vertices> <max_degree> <seed_number> (optional)<is_weighted>
+# Usage - python3 graphGenerator.py     -v <num_of_vertices>
+#                                       -d <max_degree>
+#                                       -s <seed_number>
+#                                       -w <is_weighted>                                (optional, default - unweighted)
+#                                       -f <graph_format>   [graphsim, neptune, neo4j]  (optional, default - graphsim)
 
 global_vertices = []
 global_in_vertices = []
@@ -39,9 +44,10 @@ class Vertex(object):
 def chooseSonFromIns(_vertex):
     in_vertex = random.choice(global_ins_only)
 
-    if not global_vertices[in_vertex].has_father:
-        setEdge(_vertex, in_vertex)
+    while global_vertices[in_vertex].has_father:
+        in_vertex = random.choice(global_ins_only)
 
+    setEdge(_vertex, in_vertex)
     global_ins_only.remove(in_vertex)
 
 
@@ -58,9 +64,9 @@ def chooseSons(_vertex):
 
 
 def setEdge(_vertex, curr_son_id):
-    if is_weighted:
-        weight = random.randint(1, 1000)
-    else:
+    weight = random.randint(1, 1000)
+
+    if not is_weighted:
         weight = 1
 
     _vertex.sons[curr_son_id] = weight
@@ -81,9 +87,10 @@ def main():
     # the first vertex most be out
     v_state = ['Out']
     for v_id in range(num_of_vertices):
-        if v_id % print_count == 0:
-            sys.stdout.write('{} ({}%) \r'.format(v_id, int((v_id / num_of_vertices) * 100)))
-            sys.stdout.flush()
+        if num_of_vertices > 100:
+            if v_id % print_count == 0:
+                sys.stdout.write('{} ({}%) \r'.format(v_id, int((v_id / num_of_vertices) * 100)))
+                sys.stdout.flush()
 
         # the last vertex most be in
         if v_id == num_of_vertices - 1:
@@ -101,23 +108,12 @@ def main():
         v_state = random.choices(['In', 'Out', 'Both'], [0.1, 0.1, 0.8])
         if debug: print(v_state)
 
-    print("Finish of step 1...\n\nStart step 2 (find a father for every node)")
-
+    print(
+        'Finish of step 1...\n\nStart step 2 (make sure graph is connected, associating a father for every in vertex)')
     # visit all the vertices that can get In edge, and associating a father....
-    for i, vertex in enumerate(global_vertices):
-        if i % print_count == 0:
-            sys.stdout.write('{} ({}%) \r'.format(i, int((i / num_of_vertices) * 100)))
-            sys.stdout.flush()
-
-        if vertex.state == ['Out'] or vertex.state == ['Both']:
-            # means that node have son or more
-            chooseSons(vertex)
-
-    print('Finish of step 2...\n\nStart step 3 (make sure graph is connected)')
     while len(global_ins_only) > 0:
         sys.stdout.write('{} in vertices steel not verified as connected.. \r'.format(len(global_ins_only)))
         sys.stdout.flush()
-
         vertex = random.choice(global_vertices)
         chooseSonFromIns(vertex)
 
@@ -128,14 +124,28 @@ def main():
     print('num of in vertices that not has father: {} ({} %)'.format(ins_without_father, (
                 (ins_without_father / len(global_vertices)) * 100)))
 
-    print('Finish of step 3...\n\nStart step 4 (save to file)')
-
-    # save in edges.csv file (GraphSim and Neo4j input)
-    with open(out_filename + '.g', 'w') as outFile:
-        for i, vertex in enumerate(global_vertices):
+    print("Finish of step 2...\n\nStart step 3 (find a sons for every out node till max degree)")
+    for i, vertex in enumerate(global_vertices):
+        if num_of_vertices > 100:
             if i % print_count == 0:
                 sys.stdout.write('{} ({}%) \r'.format(i, int((i / num_of_vertices) * 100)))
                 sys.stdout.flush()
+
+        if vertex.state == ['Out'] or vertex.state == ['Both']:
+            # means that node have son or more
+            chooseSons(vertex)
+
+    print('Finish of step 3...\n\nStart step 4 (save to file)')
+    # save in edges.csv file (GraphSim and Neo4j input)
+    with open(out_filename, 'w') as outFile:
+        if graph_format == 'neptune':
+            outFile.write('~id, ~from, ~to, weight:Double\n')
+
+        for i, vertex in enumerate(global_vertices):
+            if num_of_vertices > 100:
+                if i % print_count == 0:
+                    sys.stdout.write('{} ({}%) \r'.format(i, int((i / num_of_vertices) * 100)))
+                    sys.stdout.flush()
 
             ordered_sons = collections.OrderedDict(sorted(vertex.sons.items()))
             for son, weight in ordered_sons.items():
@@ -146,7 +156,7 @@ def main():
     outFile.close()
     print('Finish step 4')
 
-    if debug2:
+    if debug:
         # print the vertices and their in and out degree
         for v in global_vertices:
             print('\n\n\nVertex id: {}'.format(v.id).ljust(20), 'state: {}'.format(v.state).ljust(10),
@@ -200,27 +210,77 @@ def main():
         nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=10)
         nx.draw_networkx_labels(G, pos, font_size=10, font_family='sans-serif')
         plt.show()
-        # plt.savefig(<save_dir>)
+        # plt.savefig(<wherever>)
 
 
 if __name__ == "__main__":
-    debug, debug2 = 0, 0
+    debug = 0
     plot = 0
 
     argv = sys.argv[1:]
     is_weighted = False
-    num_of_vertices = int(argv[0])
-    max_degree = int(argv[1])
-    seed_num = int(argv[2])
+    graph_format = 'graphsim'
 
-    if len(argv) > 3:
-        is_weighted = True
+    print(argv)
+    for i, arg in enumerate(argv):
+        if '-v' == arg:
+            num_of_vertices = int(argv[i + 1])
+        if '-d' == arg:
+            max_degree = int(argv[i + 1])
+        if '-s' == arg:
+            seed_num = int(argv[i + 1])
+        if '-w' == arg:
+            is_weighted = True
+        if '-f' == arg:
+            graph_format = argv[i + 1].lower()
+            # 'graphsim' or 'neptune' or 'neo4j'
 
     out_filename = 'graphs/edges{}-deg{}-seed{}'.format(num_of_vertices, max_degree, seed_num)
     if is_weighted:
         out_filename += '-weighted'
     else:
         out_filename += '-unweighted'
+
+    if graph_format == 'neptune' or graph_format == 'neo4j':
+        out_dir = out_filename + '-' + graph_format
+        file_suffix = 1
+        while os.path.exists(out_dir):
+            out_dir = out_filename + '-' + graph_format + '_' + str(file_suffix)
+            file_suffix += 1
+        out_filename = out_dir
+        os.mkdir(out_dir)
+
+        if graph_format == 'neptune':
+            vertexFile = open(out_dir + '/vertex.csv', 'w')
+            vertexFile.write('~id\n')
+            out_filename += ('/edge.csv')
+
+        else:
+            nodesHeaderFile = open(out_dir + '/nodes_headers.csv', 'w')
+            nodesHeaderFile.write('nodes:ID\n')
+            nodesHeaderFile.close()
+
+            edgersHeaderFile = open(out_dir + '/edges_headers.csv', 'w')
+            edgersHeaderFile.write(':START_ID,:END_ID,:TYPE,dist:int\n')
+            edgersHeaderFile.close()
+
+            vertexFile = open(out_dir + '/nodes.g', 'w')
+            out_filename += ('/edges.g')
+
+        vertices_list = range(num_of_vertices)
+        for vertex in vertices_list:
+            vertexFile.write(str(vertex) + '\n')
+        vertexFile.close()
+
+    else:
+        out_filename += ('-' + graph_format + '.g')
+
+        tmp_filename = out_filename
+        file_suffix = 1
+        while os.path.exists(tmp_filename):
+            tmp_filename = out_filename[:-2] + '_' + str(file_suffix) + out_filename[-2:]
+            file_suffix += 1
+        out_filename = tmp_filename
 
     start = time.time()
     main()
